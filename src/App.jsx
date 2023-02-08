aafc310d58e721c58ddeec6438951be41245bc80
Darksied95 <aliyuefe9501@gmail.com>
2023-02-08 18:05:39 +0100

    switched to tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Heading from "./Components/Heading";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="bg-zinc-900">
+      <Heading />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Heading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Moon from "../assets/icon-moon.svg";
+import Sun from "../assets/icon-sun.svg";
+
+const Heading = () => {
+  return (
+    <div>
+      <p>Todo</p>
+      <img src={Sun} />
+    </div>
+  );
+};
+
+export default Heading;
