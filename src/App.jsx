89aa7c9b9cd658c381e03a7f5d2fbce543bb693c
Darksied95 <aliyuefe9501@gmail.com>
2023-02-10 16:31:59 +0100

    changed backgroundImage from theme to theme.extend

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import Todo from "./Components/Todo";
 import bgDesktop from "./assets/bg-desktop-dark.jpg";
 
 function App() {
-	const [show, setShow] = React.useState(true);
-	return (
-		<div
-			className={`bg-dark-vdb min-h-screen w-full bg-hero-pattern bg-no-repeat bg-[length:100%_30%]`}
-		>
-			<Todo />
-		</div>
-	);
-
+  return (
+    <div
+      className={`bg-dark-vdb min-h-screen w-full bg-hero-pattern bg-no-repeat bg-[length:100%_30%]`}
+    >
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
