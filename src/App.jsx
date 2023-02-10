2e58e324a02196150c2d5bd77083f1d2c7d4c5cd
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-10 15:27:41 +0100

    background image and max width set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Todo from "./Components/Todo";
+import bgDesktop from "./assets/bg-desktop-dark.jpg";
 
 function App() {
 	const [show, setShow] = React.useState(true);
 	return (
-		<div className="bg-dark-vdb min-h-screen w-full">
+		<div
+			className={`bg-dark-vdb min-h-screen w-full bg-hero-pattern bg-no-repeat bg-[length:100%_30%]`}
+		>
 			<Todo />
 		</div>
 	);
