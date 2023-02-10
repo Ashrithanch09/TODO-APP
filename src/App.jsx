358d9ea2802c56ee84099f35a544b16336b5ff24
Tamarapreye-M <preyemichael116@gmail.com>
2023-02-10 14:55:42 +0100

    background color implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import "./App.css";
 import Todo from "./Components/Todo";
 
 function App() {
-  return (
-    <div className="max-w-[700px] bg-light-vlgb">
-      <Todo />
-    </div>
-  );
+	const [show, setShow] = React.useState(true);
+	return (
+		<div className="bg-dark-vdb min-h-screen w-full">
+			<Todo />
+		</div>
+	);
 }
 
 export default App;
