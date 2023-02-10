6df9f9ec7dd3f089a2dadb06ed97c22ab7d11493
segunmotunde <sogunledun@gmail.com>
2023-02-10 21:34:45 +0100

    done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>Drag and drop to reorder list </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <input type="checkbox" name="" id="" />
+      <input type="text" name="todo" id="" />
+    </div>
+  )
+}
+
+export default Main
