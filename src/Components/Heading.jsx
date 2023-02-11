a4398a40a90071b218b31d7dc4786995670d3916
segunmotunde <sogunledun@gmail.com>
2023-02-11 01:12:54 +0100

    done my task

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Moon from "../assets/icon-moon.svg";
 import Sun from "../assets/icon-sun.svg";
 
 const Heading = () => {
+  const [mode, setMode] = useState(true);
   return (
     <div className=" flex justify-between items-center font-bold mb-5">
-      <p className="text-3xl tracking-widest uppercase text-light-vlg ">Todo</p>
-      <img src={Sun} className="w-8" />
+      <p className="text-4xl tracking-[1.5rem] uppercase text-light-vlg ">Todo</p>
+      <span onClick={() => setMode(!mode)}>
+        {mode ? <img src={Sun} className="w-8" />:  <img src={Moon} className="w-8" />}
+      </span>
+      
+     
+
     </div>
   );
 };
