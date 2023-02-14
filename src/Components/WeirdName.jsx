201680e78f69e61db9a31e43a564b3e280938921
Darksied95 <aliyuefe9501@gmail.com>
2023-02-14 16:34:24 +0100

    add detachment in others component

--- src/Components/WeirdName.jsx
+++ src/Components/WeirdName.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const WeirdName = () => {
-  return <div>WeirdName</div>;
+  return (
+    <div className="flex justify-between text-light-lgb bg-dark-vdsb py-4 px-4 relative">
+      <p>5 items left</p>
+      <div className="flex top-20 left-0 right-0 absolute ml-0 justify-center py-4 rounded-md bg-dark-vdsb md:static md:py-0 md:ml-9 md:justify-between ">
+        <p className="mr-4"> All</p>
+        <p className="mr-4">Active</p>
+        <p className="mr-4">Completed</p>
+      </div>
+      <p>Clear Completed</p>
+    </div>
+  );
 };
 
 export default WeirdName;
